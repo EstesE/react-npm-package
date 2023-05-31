d789fcccdefdce21a5468117e0c736ff303b4449
Mithrilhall <eric_estes@aspensquare.com>
2023-05-31 14:08:44 -0400

    npx sb init - now checking in the results

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,17 @@
-// module.exports = {
-//     //   stories: ['./*.stories.js'],
-//     stories: ['./**/*.stories.@(js|jsx|ts|tsx)'],
-//     // stories: ['./Component.stories.js'],
-//     framework: {
-//         name: '@storybook/react-webpack5',
-//         options: {}
-//     },
-//     docs: {
-//         autodocs: true
-//     }
-// };
-
-
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-    stories: ['../src/**/*.stories.@(js|jsx)'],
-    addons: [],
-    framework: {
-        name: "@storybook/react-webpack5",
-        options: {},
-    },
-    features: {
-        interactionsDebugger: true,
-    },
-    docs: {
-        autodocs: true,
-    },
-    staticDirs: ["../public"],
-}
-
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
 export default config;
